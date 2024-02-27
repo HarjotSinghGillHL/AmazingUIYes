fe7640f14c6ca52938d747dbb76b5db84a8dd14f
Harjot <121034602+ChasingTimefall@users.noreply.github.com>
2024-02-27 10:30:04 -0400

    Finished

--- Assets/Scripts/HL_UserInterface.cs
+++ Assets/Scripts/HL_UserInterface.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public enum EButtonPressEvent : int
 {
     BEVENT_ADD_SCORE = 0,
     BEVENT_SUB_SCORE,
     BEVENT_RESET_SCORE,
+    BEVENT_RANDOM_SCORE,
+    BEVENT_SHOWHIDE_KEY,
+    BEVENT_EXIT_GAME,
     BEVENT_MAX,
 }
 public class HL_UserInterface : MonoBehaviour
@@ -16,18 +18,39 @@ public class HL_UserInterface : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject ScoreTextGameObject = null;
+    public GameObject KeyImageGameObject = null;
+    public GameObject RgbaTestImageGameObject = null;
+
+    private Color colRgbaTestImage = Color.white;
 
     TextMeshProUGUI ScoreTextObject = null;
+    UnityEngine.UI.Image RgbaTestImage = null;
+
     int Score = 0;
     void Start()
     {
         ScoreTextObject= ScoreTextGameObject.GetComponent<TextMeshProUGUI>();
+        RgbaTestImage = RgbaTestImageGameObject.GetComponent<UnityEngine.UI.Image>();
         ScoreTextObject.SetText(Score.ToString());
     }
-
     private void Awake()
     {
 
+    }
+    public void OnSliderValueUpdate(UnityEngine.UI.Slider _Slider)
+    {
+        _Slider.gameObject.transform.Find("ValueText").GetComponent<TextMeshProUGUI>().SetText(((int)(_Slider.value * 255f)).ToString());
+        
+        if (_Slider.gameObject.name == "R")
+            colRgbaTestImage.r = _Slider.value;
+        else if (_Slider.gameObject.name == "G")
+            colRgbaTestImage.g = _Slider.value;
+        else if (_Slider.gameObject.name == "B")
+            colRgbaTestImage.b = _Slider.value;
+        else if (_Slider.gameObject.name == "A")
+            colRgbaTestImage.a = _Slider.value;
+
+        RgbaTestImage.color = colRgbaTestImage;
     }
 
     public void OnButtonEvent(int Event)
@@ -36,12 +59,12 @@ public class HL_UserInterface : MonoBehaviour
         {
             case EButtonPressEvent.BEVENT_ADD_SCORE:
                 {
-                    Score += 15;
+                    Score += 100000000;
                     goto LABEL_SCORE_UPDATE;
                 }
             case EButtonPressEvent.BEVENT_SUB_SCORE:
                 {
-                    Score -= 15;
+                    Score -= 100000000;
                     goto LABEL_SCORE_UPDATE;
                 }
             case EButtonPressEvent.BEVENT_RESET_SCORE:
@@ -49,20 +72,45 @@ public class HL_UserInterface : MonoBehaviour
                     Score = 0;
                     goto LABEL_SCORE_UPDATE;
                 }
+            case EButtonPressEvent.BEVENT_RANDOM_SCORE:
+                {
+                    Score += Random.Range(-999999999, 999999999);
+                    goto LABEL_SCORE_UPDATE;
+                }
+            case EButtonPressEvent.BEVENT_SHOWHIDE_KEY:
+                {
+                    KeyImageGameObject.SetActive(!KeyImageGameObject.activeSelf); 
+                    goto LABEL_POST_PROCESS_EVENT;
+                }
+            case EButtonPressEvent.BEVENT_EXIT_GAME:
+                {
+                    goto LABEL_POST_PROCESS_EVENT_ON_EXIT;
+                }
             default:
                 {
                     //Invalid Event
                     return;
                 }
         }
+    LABEL_POST_PROCESS_EVENT:
 
-LABEL_PROCESS_EVENT:
 
         return;
 
-LABEL_SCORE_UPDATE:
+    LABEL_POST_PROCESS_EVENT_ON_EXIT:
+
+        Application.Quit();
+        return;
+
+    LABEL_SCORE_UPDATE:
+
+        if (Score < 0)
+            Score = 0;
+        else if (Score > 999999999)
+            Score = 999999999;
+        
         ScoreTextObject.SetText(Score.ToString());
-        goto LABEL_PROCESS_EVENT;
+        goto LABEL_POST_PROCESS_EVENT;
 
     }
 
